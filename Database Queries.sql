edec4ef0ba9bbe663270911e2f193b4220c69ffd
Holiday Yo Mothowamodimo <mr.mothowamodimo@gmail.com>
2025-06-17 21:17:52 +0200

    Add files via upload

---
+++ Database Queries.sql
@@ -0,0 +1,32 @@
+--@Yo Mothowamodimo
+-- Queries used to test out the database
+
+-- Selects all Books and their authors
+SELECT B.BookTitle, A.FirstName, A.LastName
+FROM BOOK B
+JOIN AUTHOR A ON B.AuthorID = A.AuthorID;
+
+-- Lists all members of library and the books they loaned
+SELECT M.FirstName, M.LastName, B.BookTitle, L.LoanDate, L.ReturnDate
+FROM MEMBERS M
+JOIN LOAN L ON M.MembersID = L.MembersID
+JOIN BOOK B ON L.BookID = B.BookID;
+
+-- List all Branches and the number of books they have
+SELECT BR.Branchname, BK.BookTitle, BL.Quantity
+FROM BOOKLOCATION BL
+JOIN BRANCHES BR ON BL.BranchID = BR.BranchID
+JOIN BOOK BK ON BL.BookID = BK.BookID;
+
+-- List Staff and which branch they work at
+SELECT S.StaffName, S.StaffSurname, B.Branchname
+FROM STAFF S
+JOIN BRANCHES B ON S.BranchID = B.BranchID;
+
+-- List the names of members who have overdue loans
+SELECT M.FirstName, M.LastName, B.BookTitle, L.ReturnDate
+FROM LOAN L
+JOIN MEMBERS M ON L.MembersID = M.MembersID
+JOIN BOOK B ON L.BookID = B.BookID
+WHERE L.ReturnDate < '2025-06-17';
+
